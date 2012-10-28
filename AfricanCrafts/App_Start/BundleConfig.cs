06faf15b5ed0d5b5d204a8373d927798217fcb5f
Devlin Schoonraad <devlin.schoonraad@gmail.com>
2012-10-28 23:30:40 +0200

    Added font-awesom
    Added font-awesome, and added some samples.
    Some assembly required...

--- AfricanCrafts/App_Start/BundleConfig.cs
+++ AfricanCrafts/App_Start/BundleConfig.cs
@@ -17,7 +17,8 @@ namespace AfricanCrafts
                 ));
 
             bundles.Add(new StyleBundle("~/content/bootstrap").Include(
-                "~/Content/bootstrap.css"
+                "~/Content/bootstrap.css",
+                "~/Content/font-awesome.css"
                 ));
             bundles.Add(new StyleBundle("~/content/css-responsive").Include(
                 "~/Content/bootstrap-responsive.css"
@@ -32,7 +33,7 @@ namespace AfricanCrafts
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
-
+                        
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
